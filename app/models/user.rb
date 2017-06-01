b25eb809e8de168d9b36db0d4d27c0c400cf9232
David Waite <dwaite498@gmail.com>
2017-06-01 17:39:18 +0000

    bug fix unable to log in

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,21 +1,24 @@
 class User < ActiveRecord::Base
   attr_accessor :login
+  validates_format_of :username, with: /^[a-zA-Z0-9_\.]*$/, :multiline => true
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   
-  def self.find_first_by_auth_conditions(warden_conditions)
-    conditions = warden_conditions.dup
-    if login = conditions.delete(:login)
-      where(conditions).where(["lower(username) = :value OR lower(email) = :value", { :value => login.downcase }]).first
-    else
-      if conditions[:username].nil?
-        where(conditions).first
-      else
-        where(username: conditions[:username]).first
+  def self.find_for_database_authentication(warden_conditions)
+      conditions = warden_conditions.dup
+      if login = conditions.delete(:login)
+        where(conditions.to_h).where(["lower(username) = :value OR lower(email) = :value", { :value => login.downcase }]).first
+      elsif conditions.has_key?(:username) || conditions.has_key?(:email)
+        where(conditions.to_h).first
       end
     end
+  
+  def validate_username
+    if User.where(email: username).exists?
+      errors.add(:username, :invalid)
+    end
   end
-    
+  
   validates :username,
   :presence => true,
   :uniqueness => {
@@ -24,11 +27,7 @@ class User < ActiveRecord::Base
   
   validate :validate_username
 
-  def validate_username
-    if User.where(email: username).exists?
-      errors.add(:username, :invalid)
-    end
-  end
+
   
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
